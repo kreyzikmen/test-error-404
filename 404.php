cb518eebf7f6c45ffe83bcdc4c6806e647f4285b
kreyzikmen <122941360+kreyzikmen@users.noreply.github.com>
2023-04-21 15:52:46 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 Page Not Found</title>
+   
+</head>
+<body>
+   <div class="wrapper">
+      <section class="block-error">
+         <div class="container">
+            <div class="contend">
+               <h1 class="block-error__slogan">404 Page Not Found</h1>
+               <p class="block-error__text">We're sorry, the page you requested could not be found. Please check the URL and try again.</p>
+   
+            </div>
+   
+         </div>
+
+      </section>
+      
+   </div>
+
+
+   <style>
+      body{
+         background-color: #272727;
+         color: #ffff;
+      }
+      .block-error{
+         display: flex;
+         justify-content: center;
+         align-items: center;
+
+
+         height: 100vh;
+      }
+      .block-error__slogan{
+         font-size: 100px;
+         color: rgb(255, 255, 255);
+      }
+      .block-error__text{
+         
+        
+      }
+
+      .contend{
+         border: solid 1px;
+         border-radius:  30px;
+         height: 400px;
+         width: 1000px;
+         text-align: center;
+      }
+      .container{
+         max-width: 1200px;
+         margin: 0 auto;
+         
+
+      }
+   </style>
+</body>
+</html>
